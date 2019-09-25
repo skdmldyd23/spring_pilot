80c5a3ae5c08cb16969814fd14c0335d99027413
skdmldyd23 <skdmldyd23@naver.com>
2019-09-25 18:32:04 +0900

    maybe v0.0.5

--- src/main/java/com/ryeom/demo/HomeController.java
+++ src/main/java/com/ryeom/demo/HomeController.java
@@ -1,43 +1,29 @@
 package com.ryeom.demo;
-
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import javax.annotation.Resource;
 import javax.inject.Inject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.ryeom.demo.board.Service.BoardService;
-import com.ryeom.demo.user.Service.UserService;
 
-/**
- * 마 도란나 정지워이 ㅡㅡ
- */
 @Controller
 public class HomeController {
-<<<<<<< HEAD
 	
 	@Resource(name="boardService")
-=======
 
 	@Inject
->>>>>>> 439453782072bd26dbb2fe778ddb9a6f53035a24
 	private BoardService boardService;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Model model) {
 
 		model.addAttribute("boardtest", boardService.boardTest());
-		System.out.println("이이이");
+		System.out.println("index입니다.");
 
 		// System.out.println(userService.Test().toString());
 		return "index";
 	}
 }
+//asdasdsadasd
